c4ec82d1d88617e9fd421f45668dd6bdb850709b
ajpadhiyar18 <abhaypadhiyar01@gmail.com>
2025-12-29 09:45:43 +0530

    Js Basics

---
+++ 01_basics/Day01/variables.js
@@ -0,0 +1,16 @@
+const accountId = 145642
+let accountEmail = "abhay@gmail.com"
+var accountPassword = "abhay@1234"
+accountCity= "Mumbai"
+let accountState = "";
+
+// accountId = 123456 not allowed
+
+accountEmail = "abhhy@gmail.com"
+accountPassword = "abhay@5678"
+accountCity= "Pune"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+// console.log(accountId);
