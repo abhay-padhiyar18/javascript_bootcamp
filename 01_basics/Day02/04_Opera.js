a3ed8fab737df0d506c22d14cd1202c9729f26ce
ajpadhiyar18 <abhaypadhiyar01@gmail.com>
2025-12-30 15:24:10 +0530

    converionofoperations

---
+++ 01_basics/Day02/04_Opera.js
@@ -0,0 +1,27 @@
+// let value = 3
+// let negValue = -value;
+
+// console.log(negValue); // -3
+
+// console.log("1" + 2);
+// console.log(1+"2");
+
+// console.log("1"+2 +2);
+// console.log(3+2 +"2");
+
+// console.log((3+4) * 5 %3);
+
+// console.log(true + 15); // 1
+
+// Assignment Operators
+let num1, num2, num3;
+
+num1 = num2 = num3 = 5 + 5;
+
+let gameCounter = 100;
+++gameCounter; // gameCounter = gameCounter + 1
+console.log(gameCounter); // 101
+
+
+//prefix vs postfix -> mdn docs
+ let
